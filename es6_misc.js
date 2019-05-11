c4fda76149cea8b65ca3b93121852d03df991e4e
Jinto Jose <jintoppy@gmail.com>
2019-05-11 13:00:48 +0530

    destructuring

---
+++ es6_misc.js
@@ -0,0 +1,40 @@
+const obj = {
+    abc: 'adsf',
+    def: 'aaa',
+    inner: 'adsf'
+};
+// const abc = obj.abc;
+// const def = obj.def;
+const {  abc, def } = obj;
+
+console.log(abc);
+console.log(def);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const person = {
+//     name: 'Jss',
+//     age: 10
+// };
+// const str = 'Your name is ' + person.name + 'and your age is ' + person.age +
+//     'and something else';
+
+// const str1 = `Your 'name' is ${person.name} and your age is ${person.age}
+//     and something else
+// `;
+
+// console.log(str1);
+
+
+
+
